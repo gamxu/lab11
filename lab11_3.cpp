5d6ff93769bc070788d02c8611aa0c92de670a22
gamxu <wuttipat.did@gmail.com>
2021-12-17 14:29:55 +0700

    lab11_3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int count;
+    double sum,mean,sumofsqr,sdfront,SD;
+    string num;
+    ifstream source("score.txt");
+
+    while(getline(source,num)){
+        sumofsqr += pow(stod(num),2);
+        sum += stod(num);
+        count++;
+    }
+    
+    sdfront = sumofsqr/count;
+    mean = sum/count;
+    SD = sqrt(sdfront-pow(mean,2));
+    
+    cout << "Number of data = " << count <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean <<"\n";
+    cout << "Standard deviation = " << SD <<"\n";
+}
